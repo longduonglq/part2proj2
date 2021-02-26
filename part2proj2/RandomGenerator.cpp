6542d4c48af0cc17026d2297f940e9ca21ad0c0e
Christopher <programmerchristopher@gmail.com>
2021-02-26 15:21:49 -0800

    setSeed() & getRandomInt()

--- part2proj2/RandomGenerator.cpp
+++ part2proj2/RandomGenerator.cpp
@@ -1,18 +1,22 @@
+// Participants: Christopher
+// Date: 02-28-21
+// Description: Chapter 2 - Random Generator
+#include <time.h> 
 #include "RandomGenerator.h"
 
 RandomGenerator::RandomGenerator(long seed, long multiplier, long increment, long modulus)
 {
-
+	//buffer
 }
 
 void RandomGenerator::setSeed(long newSeed)
 {
-
+	seed = newSeed;
 }
 
 long RandomGenerator::getRandomInt()
 {
-
+	return (multiplier * seed + increment) % modulus;
 }
 
 double RandomGenerator::getRandomInUnitInterval()
